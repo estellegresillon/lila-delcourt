a81f66c31f190789b619a63ee48c7aa59d981cef
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-13 23:11:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.scss';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className="logo-link">
+          Lila Delcourt
+          <div className="underline" />
+        </div>
+        <div className="menu-item galery-link">
+          Projects
+          <div className="underline" />
+        </div>
+        <div className="menu-item menu-link">
+          Menu
+          <div className="underline" />
+        </div>
       </header>
-    </div>
+
+      <div className="hero-scene">
+        <div className="title">Hello, I am a photographer based in <span className="overline">Paris</span> and traveling around the world.</div>
+        <i className="fas fa-long-arrow-alt-down" />
+        <video muted loop autoPlay id="myVideo">
+          <source src="home4.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <div className="section-presentation">
+        <div className="presentation-left">
+          We don't trust words, we trust pictures.
+        </div>
+        <div className="presentation-right">
+          <div className="separator" />
+          I do not know what I may appear to the world, but to myself I seem to have been only like a boy 
+          playing on the seashore, and diverting myself in now and then finding a smoother pebble or a prettier 
+          shell than ordinary, whilst the great ocean of truth lay all undiscovered before me.
+        </div>
+      </div>
+
+      <div className="section-photos">
+        <div className="photo-left">
+          <img src="hero1.jpg" alt="nature-left" />
+        </div>
+        <div className="photo-right">
+          <img src="hero2.jpg" alt="nature-right" />
+        </div>
+      </div>
+    </>
   );
 }
 
