6d9ad260438932c6b739221149e04456b7c4404a
Estelle GRESILLON <estelle.gresillon@gmail.com>
2020-01-14 14:28:07 +0100

    finished home layout

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
     <>
       <header>
         <div className="logo-link">
-          Lila Delcourt
+          Romain Delcourt
           <div className="underline" />
         </div>
         <div className="menu-item galery-link">
@@ -21,7 +21,8 @@ const App = () => {
       </header>
 
       <div className="hero-scene">
-        <div className="title">Hello, I am a photographer based in <span className="overline">Paris</span> and traveling around the world.</div>
+        <div className="title">Hello, I am a photographer based in <span className="overline">Paris</span>
+          . I am inspired by <span className="overline">nature</span> and traveling around the <span className="overline">world</span>.</div>
         <i className="fas fa-long-arrow-alt-down" />
         <video muted loop autoPlay id="myVideo">
           <source src="home4.mp4" type="video/mp4" />
@@ -34,8 +35,8 @@ const App = () => {
         </div>
         <div className="presentation-right">
           <div className="separator" />
-          I do not know what I may appear to the world, but to myself I seem to have been only like a boy 
-          playing on the seashore, and diverting myself in now and then finding a smoother pebble or a prettier 
+          I do not know what I may appear to the world, but to myself I seem to have been only like a boy
+          playing on the seashore, and diverting myself in now and then finding a smoother pebble or a prettier
           shell than ordinary, whilst the great ocean of truth lay all undiscovered before me.
         </div>
       </div>
@@ -46,6 +47,18 @@ const App = () => {
         </div>
         <div className="photo-right">
           <img src="hero2.jpg" alt="nature-right" />
+        </div>
+      </div>
+
+      <div className="fullscreen-img">
+        <img src="/project1-min.jpg" alt="project1" />
+      </div>
+
+      <div className="section-presentation">
+        <div className="presentation-center">
+          I do not know what I may appear to the world, but to myself I seem to have been only like a boy
+          playing on the seashore, and diverting myself in now and then finding a smoother pebble or a prettier
+          shell than ordinary, whilst the great ocean of truth lay all undiscovered before me.
         </div>
       </div>
     </>
