4e96044f80265aabfc60893f5d8f20abb37f3e17
Estelle GRESILLON <estelle.gresillon@gmail.com>
2020-01-15 15:29:27 +0100

    added react-waypoint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Waypoint } from 'react-waypoint';
 
 import './App.scss';
 
 const App = () => {
+  const handleWaypointEnter = () => {
+    const el = document.querySelector(".third-section");
+    el.classList.add("transition-on");
+  }
+
+  const handleWaypointLeave = () => {
+    const el = document.querySelector(".third-section");
+    el.classList.remove("transition-on");
+  }
+
   return (
     <>
       <header>
@@ -49,17 +60,21 @@ const App = () => {
       </section>
 
       <div className="parallax" />
-
       <section className="section-presentation">
-        <div className="presentation-center">
-          You don't make a photograph just with a camera.
-          You bring to the act of photography all the pictures you have seen, the books you have read,
-          the music you have heard, the people you have loved.
-          When you photograph people in color, you photograph their clothes.
-          But when you photograph people in Black and white, you photograph their souls!
+        <Waypoint
+          onEnter={() => handleWaypointEnter()}
+          onLeave={() => handleWaypointLeave()}
+        >
+          <div style={{}} className="presentation-center third-section">
+            You don't make a photograph just with a camera.
+            You bring to the act of photography all the pictures you have seen, the books you have read,
+            the music you have heard, the people you have loved.
+            When you photograph people in color, you photograph their clothes.
+            But when you photograph people in Black and white, you photograph their souls!
         </div>
+        </Waypoint>
       </section>
-      <footer><div className="logo-link">Romain Delcourt</div></footer>
+      <footer><div className="logo-link">© Romain Delcourt - Tous droits réservés - Mentions légales</div></footer>
     </>
   );
 }
